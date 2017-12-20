655dde0a3874a1570b4089fd99b88d8f890174cf
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2017-12-20 19:56:20 -0200

    App - Clone WhatsApp, criando autenticação de usuário com FireBase, usando BASE64

---
+++ CloneWhatsApp/app/src/main/java/io/whatsapp/clonewhatsapp/helpers/Preferencias.java
@@ -0,0 +1,36 @@
+package io.whatsapp.clonewhatsapp.helpers;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.HashMap;
+
+public class Preferencias {
+
+    private Context contexto;
+    private SharedPreferences preferences;
+    private final String NOME_ARQUIVO = "whatsapp.preferencias";
+    private final int MODE = 0;
+    private SharedPreferences.Editor editor;
+
+    private final String CHAVE_UID = "identificadorUsuarioLogado";
+
+    public Preferencias( Context contextoParametro){
+
+        contexto = contextoParametro;
+        preferences = contexto.getSharedPreferences(NOME_ARQUIVO, MODE );
+        editor = preferences.edit();
+
+    }
+
+    public void salvarDados( String uidUser){
+
+        editor.putString(CHAVE_UID, uidUser);
+
+        editor.commit();
+
+    }
+
+
+
+}
